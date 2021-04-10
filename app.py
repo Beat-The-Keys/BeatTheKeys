a677d782a115dccb4f33efb15dc1360d0db462d3
Akash Patel <ap.akash.patel1999+github@gmail.com>
2021-04-10 02:42:41 +0000

    testing out rooms

--- app.py
+++ app.py
@@ -1,14 +1,50 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, json
+from flask_socketio import SocketIO, join_room, leave_room
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder='./build/static')
 
+
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app,
+                    cors_allowed_origins="*",
+                    json=json,
+                    manage_session=False)
 
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
 
+
+# When a client connects from this Socket connection, this function is run
+@socketio.on('connect')
+def on_connect():
+    '''When someone connects to the server'''
+    print('User connected!')
+
+
+# When a client disconnects from this Socket connection, this function is run
+@socketio.on('disconnect')
+def on_disconnect():
+    '''When someone disconnects to the server'''
+    print('User disconnected!')
+
+@socketio.on('send')
+def getSend(data):
+    socketio.emit('send', {data}, broadcast=True)
+
+# server side code
+@socketio.on('create')
+def getRoom(data):
+    join_room(data['room'])
+    socketio.emit('create', {'msg': "New user joined" + data['room']}, broadcast=True, room=data['room'])
+
+@socketio.on('leave')
+def getLeave(data):
+    leave_room(data['room'])
+    socketio.send({'msg': "user left" + data['room']}, room=data['room'])
 
 app.run(
     host=os.getenv('IP', '0.0.0.0'),
--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState, useEffect, useRef } from 'react';
+// import './styles/App.css';
+import io from 'socket.io-client';
+
+export const socket = io(); // Connects to socket connection
 
 function App() {
+  const user = useRef(null);
+  const msg = useRef(null);
+
+  const [mssg, setMssg] = useState('')
+
+  const joinRoom = ()=>{
+    // let newroom = user.current.value
+    // socket.emit('leave', {room:user.})
+    socket.emit('create', {room: user.current.value});
+  }
+  useEffect(()=>{
+    socket.on('create', (data)=>{
+      setMssg(prev=>prev+data.msg+"\n")
+      console.log("hello", data)
+    })
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input ref={user}></input>
+      <button onClick={joinRoom}>Submit</button>
+
+      <input ref={msg}></input>
+      <button onClick={mssg}>Submit</button>
+
+      <li>{mssg}</li>
+      <h1>End</h1>
+
     </div>
   );
 }
