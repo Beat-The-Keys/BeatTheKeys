5322b22b33b252e97362ed022f551ff9ed739d73
Akash Patel <ap.akash.patel1999+github@gmail.com>
2021-04-13 02:52:56 +0000

    Adding a toast for current user

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from 'react';
 import { socket } from './App';
-import Multiplayer from './Multiplayer.js'
+import GamePage from './GamePage.js'
 
 export default function Home({userName}) {
     const [joinMulti, isJoinMulti] = useState(false)
@@ -9,39 +9,14 @@ export default function Home({userName}) {
   return (
     <div>
       {joinMulti
-          ? <Multiplayer userName="Akash" room={room}/>
-          : <button onClick={()=>{
-                socket.emit('joinRoom', {userName, room})
-                isJoinMulti(true)}}>Join Game</button>
+          ? <GamePage userName="Akash" room={room}/>
+          : <div> "Home Page"
+            <br/>
+            <button onClick={()=>{
+              socket.emit('joinRoom', {userName, room})
+              isJoinMulti(true)}}>Join Game</button>
+            </div>
       }
     </div>
   );
 }
-//   const joinRoom = ()=>{
-//     // let newroom = user.current.value
-//     // socket.emit('leave', {room:user.})
-//     socket.emit('create', {room: user.current.value});
-//   }
-//   useEffect(()=>{
-//     socket.on('create', (data)=>{
-//       setMssg(prev=>prev+data.msg+"\n")
-//       console.log("hello", data)
-//     })
-//   })
-//   return (
-//     <div>
-//       <input ref={user}></input>
-//       <button onClick={joinRoom}>Submit</button>
-
-//       <input ref={msg}></input>
-//       <button onClick={mssg}>Submit</button>
-
-//       <li>{mssg}</li>
-//       <h1>End</h1>
-
-//     </div>
-//   );
-// }
-
-// export default App;
-
