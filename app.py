3d84e3f073f4ca9d9b0fcbdcb4a648bd280519d0
AJ <ajo28@njit.edu>
2021-04-13 04:03:54 +0000

    Fixed some linting errors, app.py has os.getenv default type is builtins.int. Expected str or None. (invalid-envvar-default) linting error still.

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
+"""This is the main app that serves as a server for all the clients"""
 import os
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='./build/static')
+APP = Flask(__name__, static_folder='./build/static')
 
 
-@app.route('/', defaults={"filename": "index.html"})
-@app.route('/<path:filename>')
+@APP.route('/', defaults={"filename": "index.html"})
+@APP.route('/<path:filename>')
 def index(filename):
+    """Tells python where our index file is that renders our React Components"""
     return send_from_directory('./build', filename)
 
 
-app.run(
+APP.run(
     host=os.getenv('IP', '0.0.0.0'),
     port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
 )
