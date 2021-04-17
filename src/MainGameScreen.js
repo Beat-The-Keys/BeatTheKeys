d5f76a33e65da7e382e17f58611d1abd58fcb0d9
Yusef Mustafa <yjm4@njit.edu>
2021-04-17 02:23:43 -0400

    Create shared game state and fix lint errors

--- src/MainGameScreen.js
+++ src/MainGameScreen.js
@@ -10,20 +10,26 @@ function MainGameScreen({playerName, room}) {
   const [highlightedStopIndex, setHighlightedStopIndex] = useState(0); // State for keeping track of the last character index to highlight in the prompt
   const [wpm, setWpm] = useState(0); // State for calculating wpm
   const [timeLeft, setTimeLeft] = useState(60); // State for keeping track of the time so that the wpm can be calculated
-  const [typingBegan, setTypingBegan] = useState(false); // State for checking if the userr started typing
+  const [typingBegan, setTypingBegan] = useState(false); // State for checking if the user started typing
+  const [playerFinished, setPlayerFinished] = useState(false); // State for checking if the user finished the game
 
   useEffect(() => {
-    // Calculate wpm and set it in the state
-    let entries = highlightedStopIndex / 5;
-    let currentMin = (60 - timeLeft) / 60;
-    let wpm = currentMin === 0 ? 0 : Math.round(entries / currentMin);
-    setWpm(wpm);
-    socket.emit('updatePlayerStats', {'playerName': playerName, 'wpm': wpm, 'room': room});
-  }, [highlightedStopIndex, playerName, room, timeLeft])
+    if (!playerFinished) {
+      let entries = highlightedStopIndex / 5;
+      let currentMin = (60 - timeLeft) / 60;
+      let wpm = currentMin === 0 ? 0 : Math.round(entries / currentMin);
+      setWpm(wpm);
+      socket.emit('updatePlayerStats', {'playerName': playerName, 'wpm': wpm, 'room': room});
+    }
+  }, [highlightedStopIndex, playerName, room, timeLeft, playerFinished])
 
   function onTextChanged() {
     // Called when user starts typing
     setTypingBegan(true);
+    if (prompt === textboxRef.current.value) {
+      setPlayerFinished(true);
+      socket.emit('playerFinished', {'playerName': playerName, 'room': room});
+    }
     if (prompt.startsWith(textboxRef.current.value)) {
       setHighlightedStopIndex(textboxRef.current.value.length);
     }
@@ -34,7 +40,29 @@ function MainGameScreen({playerName, room}) {
     return (<p><b>{prompt.substring(0, highlightedStopIndex)}</b>{prompt.substring(highlightedStopIndex)}</p>);
   }
 
+  function gameStateJSX() {
+    // If the user began typing and the game isn't over yet, display the timer only.
+    if (typingBegan && !playerFinished) {
+      return (
+      <ReactTimer
+        start={60}
+        end={t => t === 0}
+        onTick={t => handleTime(t)}
+      >
+        {time => <span>TIMER: {time}</span>}
+      </ReactTimer>);
+    }
+    // Otherwise, if the player is not finished then we are at the start of the game.
+    if (!playerFinished) {
+      return <p>Begin typing: </p>;
+    }
+  }
+
   function handleTime(t) {
+    if (t === 0) {
+      socket.emit('playerFinished', {'playerName': playerName, 'room': room});
+      setPlayerFinished(true)
+    }
     // Decrement the timer and set the timeLeft state
     setTimeLeft(t);
     return t-1;
@@ -55,19 +83,10 @@ function MainGameScreen({playerName, room}) {
           </div>
         </div>
         <div className="Text-box">
-          {typingBegan ?
-            <ReactTimer
-              start={60}
-              end={t => t === 0}
-              onTick={t => handleTime(t)}
-            >
-              {time => <span>TIMER: {time}</span>}
-            </ReactTimer>
-            : <p>Begin typing: </p>
-          }
-          <div>
-            <input type="text" name="name" ref={textboxRef} onChange={onTextChanged} />
-          </div>
+          {gameStateJSX()}
+        <div>
+          <input type="text" name="name" ref={textboxRef} onChange={onTextChanged} />
+        </div>
         </div>
       </div>
     </div>
