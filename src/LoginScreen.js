f0045b84494a7b59ca0c332711b6b10f5f162c76
Mann Patel <ec2-user@ip-172-31-80-214.ec2.internal>
2021-04-19 19:59:52 +0000

    Login Screen UI added

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import {GoogleLogin} from 'react-google-login';
 import HomeScreen from './HomeScreen.js';
 import io from 'socket.io-client';
+import './LoginUI.css';
+import { Emoji } from 'emoji-mart'
 
 export const socket = io(); // Connects to socket connection
 export const client_id = "427706489011-6rshj7squ73369r4n830rl8cch7q86f2.apps.googleusercontent.com"
@@ -25,14 +27,22 @@ export default function LoginScreen (){
     <div>
     {isLoggedIn
       ? <HomeScreen playerName={playerName} responseGoogleLogout={responseGoogleLogout}/>
-      : <div>
-          <meta name="google-signin-client_id" content={client_id}/>
-          <GoogleLogin
-          clientID={client_id}
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          cookiePolicy={'single_host_origin'}
-          />
+      : <div class="login-page">
+        <center> <h1> BEAT THE KEYS! <Emoji emoji='keyboard' set='twitter' size={40} /> </h1>  </center>
+        <center> <h5> "Multiplayer typeracing" </h5> </center>
+        <div class="form">
+          <form class="login-form">
+            <meta name="google-signin-client_id" content={client_id}/>
+                <GoogleLogin
+                buttonText="Login"
+                clientID={client_id}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+                />
+          </form>
+        </div>
+        <center> <h5> Features coming soon: <br/>"Private lobbies, achievements, etc.." </h5> </center>
         </div>
     }
     </div>
