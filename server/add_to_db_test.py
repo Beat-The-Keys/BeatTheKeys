7887a0078b1d82b1c05a9ecf7f7c478424a6857d
AJ <ajo28@njit.edu>
2021-04-20 13:09:09 +0000

    Fixed Linting errrors for test files

--- server/add_to_db_test.py
+++ server/add_to_db_test.py
@@ -1,12 +1,13 @@
+"""This test is to see if we correctly add a user to the DB"""
 import unittest
-import unittest.mock as mock
 from unittest.mock import patch
 import os
 import sys
-
-sys.path.append(os.path.abspath('../'))
 from app import user_db_check
 import models
+
+sys.path.append(os.path.abspath('../'))
+
 
 KEY_INPUT = "input"
 KEY_EXPECTED = "expected"
@@ -17,6 +18,7 @@ INITIAL_ICON = 'smiley'
 INITIAL_WPM = 60
 
 class AddToDBTest(unittest.TestCase):
+    """Main class to test DB logic"""
     def setUp(self):
         self.success_test_params = [
             {
@@ -35,33 +37,38 @@ class AddToDBTest(unittest.TestCase):
                 KEY_INPUT: 'Mann@nijt.edu',
                 KEY_EXPECTED: [INITIAL_EMAIL, 'Mann@nijt.edu'],
             },
-            
         ]
-        
-        initial_person = models.Users(username=INITIAL_USERNAME, email=INITIAL_EMAIL, icon=INITIAL_ICON, bestwpm=INITIAL_WPM)
+
+        initial_person = models.Users(username=INITIAL_USERNAME, email=INITIAL_EMAIL,
+                                      icon=INITIAL_ICON, bestwpm=INITIAL_WPM)
         self.initial_db_mock = [initial_person]
-    
+
     def mocked_db_session_add(self, username):
+        """Helper function to mock DB"""
         self.initial_db_mock.append(username)
-    
+
     def mocked_db_session_commit(self):
-        pass
-    
+        """Helper function to mock DB"""
+        pass # pylint: disable=unnecessary-pass
+
     def mocked_person_query_all(self):
+        """Helper function to mock DB"""
         return self.initial_db_mock
-    
+
     def test_success(self):
+        """Main Logic function where tests are run"""
         for test in self.success_test_params:
             with patch('app.DB.session.add', self.mocked_db_session_add):
                 with patch('app.DB.session.commit', self.mocked_db_session_commit):
                     with patch('models.Users.query') as mocked_query:
                         mocked_query.all = self.mocked_person_query_all
-    
-                        actual_result = user_db_check(test[KEY_INPUT], [INITIAL_EMAIL], [INITIAL_USERNAME])
+
+                        actual_result = user_db_check(test[KEY_INPUT], [INITIAL_EMAIL],
+                                                      [INITIAL_USERNAME])
                         expected_result = test[KEY_EXPECTED]
                         print(actual_result)
                         print(expected_result)
-                        
+
                         self.assertEqual(len(actual_result), len(expected_result))
                         self.assertEqual(actual_result, expected_result)
 
--- server/get_users_from_db_test.py
+++ server/get_users_from_db_test.py
@@ -1,12 +1,13 @@
+"""This test is to see if we correctly fetch data from the DB"""
 import unittest
-import unittest.mock as mock
 from unittest.mock import patch
 import os
 import sys
-
-sys.path.append(os.path.abspath('../'))
 from app import fetch_db
 import models
+
+sys.path.append(os.path.abspath('../'))
+
 
 KEY_INPUT = "input"
 KEY_EXPECTED = "expected"
@@ -15,41 +16,52 @@ INITIAL_USERNAME = 'user1'
 INITIAL_EMAIL = 'email@host.com'
 INITIAL_ICON = 'smiley'
 INITIAL_WPM = 60
-INITIAL_LIST = [['Alexander Ong', 'Group2 YWCC 307', 'Group 4 CS 490'], ['ajo28@njit.edu', 'group2ywcc@gmail.com', 'beat.the.keys4@gmail.com'],
-['smiley', 'grinning', 'smiling_imp'], [50, 20, 80]]
+INITIAL_LIST = [['Alexander Ong', 'Group2 YWCC 307', 'Group 4 CS 490'],
+                ['ajo28@njit.edu', 'group2ywcc@gmail.com', 'beat.the.keys4@gmail.com'],
+                ['smiley', 'grinning', 'smiling_imp'], [50, 20, 80]]
 
 class GetUserFromDBTest(unittest.TestCase):
+    """Main class to test DB logic"""
     def setUp(self):
         self.success_test_params = [
             {
                 KEY_INPUT: 'wpm',
-                KEY_EXPECTED: (['Group 4 CS 490', 'Alexander Ong', 'Group2 YWCC 307'], ['beat.the.keys4@gmail.com', 'ajo28@njit.edu', 'group2ywcc@gmail.com'],
-                ['smiling_imp', 'smiley', 'grinning'], [80, 50, 20])
+                KEY_EXPECTED: (['Group 4 CS 490', 'Alexander Ong', 'Group2 YWCC 307'],
+                               ['beat.the.keys4@gmail.com', 'ajo28@njit.edu',
+                                'group2ywcc@gmail.com'],
+                               ['smiling_imp', 'smiley', 'grinning'], [80, 50, 20])
             },
             {
                 KEY_INPUT: 'email',
-                KEY_EXPECTED: (['Alexander Ong', 'Group 4 CS 490', 'Group2 YWCC 307'], ['ajo28@njit.edu', 'beat.the.keys4@gmail.com', 'group2ywcc@gmail.com'],
-                ['smiley', 'smiling_imp', 'grinning'], [50, 80, 20])
+                KEY_EXPECTED: (['Alexander Ong', 'Group 4 CS 490', 'Group2 YWCC 307'],
+                               ['ajo28@njit.edu', 'beat.the.keys4@gmail.com',
+                                'group2ywcc@gmail.com'],
+                               ['smiley', 'smiling_imp', 'grinning'], [50, 80, 20])
             },
             {
                 KEY_INPUT: ' ',
                 KEY_EXPECTED: (INITIAL_LIST[0], INITIAL_LIST[1], INITIAL_LIST[2], INITIAL_LIST[3]),
             },
         ]
-        
-        initial_person = models.Users(username=INITIAL_USERNAME, email=INITIAL_EMAIL, icon=INITIAL_ICON, bestwpm=INITIAL_WPM)
+
+        initial_person = models.Users(username=INITIAL_USERNAME, email=INITIAL_EMAIL,
+                                      icon=INITIAL_ICON, bestwpm=INITIAL_WPM)
         self.initial_db_mock = [initial_person]
-    
+
     def mocked_db_session_add(self, username):
+        """Helper function to mock DB"""
         self.initial_db_mock.append(username)
-    
+
     def mocked_db_session_commit(self):
-        pass
-    
+        """Helper function to mock DB"""
+        pass # pylint: disable=unnecessary-pass
+
     def mocked_person_query_all(self):
+        """Helper function to mock DB"""
         return self.initial_db_mock
-    
+
     def test_success(self):
+        """Main Logic function where tests are run"""
         for test in self.success_test_params:
             with patch('app.DB.session.add', self.mocked_db_session_add):
                 with patch('app.DB.session.commit', self.mocked_db_session_commit):
