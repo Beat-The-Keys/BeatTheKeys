8329280152dc83a64cb68924ce29dd3d70e4c5aa
Akash Patel <ap.akash.patel1999+github@gmail.com>
2021-04-28 18:08:58 -0400

    fixed one of the test error, the other idk

--- src/App.test.js
+++ src/App.test.js
@@ -3,16 +3,14 @@ import userEvent from "@testing-library/user-event";
 import HomeScreen from "./home/HomeScreen";
 
 const playerName = "Group 4";
-
+const playerEmail = "beat-the-keys.njit4@gmail.com"
 test("Lobby UI rendered", () => {
-    render(<HomeScreen playerName={playerName}/>);
+    render(<HomeScreen playerName={playerName} playerEmail={playerEmail}/>);
     // Verify that the main action buttons and welcome text are rendered
     expect(screen.getByText("Start Game")).toBeInTheDocument();
     expect(screen.getByText("Join Game")).toBeInTheDocument();
     expect(screen.getByText("Achievements")).toBeInTheDocument();
-    expect(screen.getByText("Select Icon")).toBeInTheDocument();
-    expect(screen.getByText("Logout")).toBeInTheDocument();
-    expect(screen.getByText("Hi, " + playerName + "! Welcome to your lobby.")).toBeInTheDocument();
+    expect(screen.getByText(`Logged In: ${playerEmail}`)).toBeInTheDocument();
 });
 
 test("Typeracer renders after starting game", () => {
@@ -21,6 +19,6 @@ test("Typeracer renders after starting game", () => {
     userEvent.click(startGameButton);
     // Verify that the typeracer UI is displayed after clicking "Start Game"
     expect(screen.getByText("Begin typing:")).toBeInTheDocument();
-    expect(document.querySelector(".prompt-text")).toBeInTheDocument();
-    expect(document.querySelector(".player-input")).toBeInTheDocument();
+    expect(document.querySelector(".p")).toBeInTheDocument();
+    expect(document.querySelector(".bXYnqS")).toBeInTheDocument();
 });
