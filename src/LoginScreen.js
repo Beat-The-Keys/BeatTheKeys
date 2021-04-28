15b7c5167f227138fcc44b378f7187fc4717c7dc
Mann Patel <ec2-user@ip-172-31-80-214.ec2.internal>
2021-04-28 19:20:35 +0000

    Title Typing animation added

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -3,7 +3,7 @@ import {GoogleLogin} from 'react-google-login';
 import HomeScreen from './home/HomeScreen.js';
 import io from 'socket.io-client';
 import { Emoji } from 'emoji-mart'
-import styled from "styled-components";
+import styled, { keyframes }from "styled-components";
 
 export const socket = io(); // Connects to socket connection
 export const client_id = "427706489011-6rshj7squ73369r4n830rl8cch7q86f2.apps.googleusercontent.com";
@@ -31,11 +31,11 @@ export default function LoginScreen (){
     {isLoggedIn
       ? <HomeScreen playerName={playerName} playerEmail={playerEmail} responseGoogleLogout={responseGoogleLogout}/>
       : <Loginpage>
-          <h1> BEAT THE KEYS!</h1>
+          <Title>BEAT THE KEYS!</Title>
           <Emoji emoji='keyboard' set='twitter' size={40}/>
           <h5> Multiplayer typeracing game </h5>
           <Form>
-            <p>Join game:</p>
+            <h5>Join game:</h5>
             <meta name="google-signin-client_id" content={client_id}/>
                 <GoogleLogin
                 buttonText="Login"
@@ -79,3 +79,27 @@ const Form = styled.div`
   box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
   border-radius: 10px;
 `;
+
+const textAnimation = keyframes`
+  from{width: 0;}
+  to{width: 265px;}
+`;
+
+const textCursor = keyframes`
+  from{border-right-color: black;}
+  to{border-right-color: transparent;}
+`;
+
+const Title = styled.p`
+  border-right: solid 5px;
+  white-space: nowrap;
+  overflow: hidden;    
+  font-family: Trebuchet MS, sans-serif;
+  font-weight: 550;
+  font-size: 35px;
+  color: Black;
+  animation: ${textAnimation} 2s linear 1s 1 normal both,
+             ${textCursor} 800ms linear infinite;
+`;
+
+
