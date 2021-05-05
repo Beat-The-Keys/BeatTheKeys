b5fa9a9626e39f1bc5fa463ab4bfff6a0ddecaa2
Yusef Mustafa <yjm4@njit.edu>
2021-05-05 19:48:00 -0400

    fix logo in navbar and landing page

--- src/login/LoginScreen.js
+++ src/login/LoginScreen.js
@@ -36,7 +36,8 @@ export default function LoginScreen (){
       ? <HomeScreen playerName={playerName} playerEmail={playerEmail} responseGoogleLogout={responseGoogleLogout}/>
       : <div id="BTK">
             <StyledNav variant="dark" sticky="top">
-            <Navbar.Brand href="#BTK"><img src="fireKey.png" width="100" height="70"/> Beat The Keys!</Navbar.Brand>
+            <img src="fireKey.png" width="100" height="70"/>
+            <Navbar.Brand style={{"marginLeft":"-20px"}}href="#BTK"> Beat The Keys!</Navbar.Brand>
             <Nav className="mr-auto">
               <Nav.Link href="#Guide">Guide</Nav.Link>
               <Nav.Link href="#Why">Why</Nav.Link>
@@ -46,7 +47,7 @@ export default function LoginScreen (){
 
           <Loginpage>
             <Title data-text="BEAT_THE_KEYS!">BEAT_THE_KEYS!</Title>
-            <img src = 'https://img.icons8.com/ios/452/keyboard.png' alt="Loading Keyboard" width="50" height="50" />
+            <img src="fireKey.png" width="150" height="105"/>
             <Para> Multiplayer typeracing game </Para>
             <Form>
             <Popin>Start game:</Popin>
